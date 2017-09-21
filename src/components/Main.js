f305d30eeaed9a99c6b683637581240a36a708b4
jackeyyang <jackeyyang@coderedfour.com>
2017-09-22 00:17:02 +0800

    init project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,34 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 图片的json数据
+let imageDatas = require('../data/imageDatas.json');
+
+// 全部图片路径信息，获得图片路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+	for(var i = 0,j = imageDatasArr.length;i < j;i++){
+		var singleImageData = imageDatasArr[i];
+
+		singleImageData.imageURL = require('../images/'+ singleImageData.fileName);
+		imageDatasArr[i] = singleImageData;
+	}
+	return imageDatasArr;
+})(imageDatas);
+
+console.log(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        jackey.yang
-      </div>
+    	<section className="stage">
+    		<section className="img-sec">
+    			Garry
+    		</section>
+    		<nav className="controller-nav">
+    		</nav>
+    	</section>
     );
   }
 }
